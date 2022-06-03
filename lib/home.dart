24106e24ea5ef6882807e72d73d732fe96205269
sathush <sathush3@gmail.com>
2022-06-03 23:01:05 +0530

    home screen designed

--- lib/home.dart
+++ lib/home.dart
@@ -8,10 +8,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0x004242),
+      backgroundColor: Colors.blueAccent,
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 24),
         child: Column(
@@ -20,7 +21,7 @@ class _HomeState extends State<Home> {
             SizedBox(height: 50),
             Text(
               "Coding Cafe",
-              style: TextStyle(color: Color(0x7d9e9e), fontSize: 20),
+              style: TextStyle(color: Colors.white, fontSize: 20),
             ),
             SizedBox(
               height: 5,
@@ -28,13 +29,69 @@ class _HomeState extends State<Home> {
             Text(
               "Cat and Dog Classifier",
               style: TextStyle(
-                  color: Color(0x7d9e9e),
+                  color: Colors.white,
                   fontSize: 30,
                   fontWeight: FontWeight.w500),
             ),
             SizedBox(height: 50),
             Center(
-              child: ,
+              child: _loading
+                  ? Container(
+                      width: 350,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset("Assets/2.1 cat_dog_icon.png"),
+                          SizedBox(height: 40)
+                        ],
+                      ),
+                    )
+                  : Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    //camera button
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 200,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                      decoration: BoxDecoration(
+                        color: Colors.yellowAccent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Capture a Photo",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  GestureDetector(
+                    //Upload Button
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 200,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                      decoration: BoxDecoration(
+                        color: Colors.yellowAccent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Upload a Photo",
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,7 +1,6 @@
 import 'package:cat_dog_classifier/home.dart';
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
-// import 'package:splashscreen/splashscreen.dart';
 
 class MySplash extends StatefulWidget {
   const MySplash({Key? key}) : super(key: key);
@@ -17,15 +16,17 @@ class _MySplashState extends State<MySplash> {
       durationInSeconds: 5,
       navigator: Home(),
       title: Text(
-        "Cat and Dog CLassifier",
+        "Cat and Dog Classifier",
         style: TextStyle(
-            fontWeight: FontWeight.bold, fontSize: 25, color: Color(0x00fff)),
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Colors.yellowAccent),
       ),
       logo: Image.asset("Assets/2.1 cat_dog_icon.png"),
       backgroundColor: Colors.blueAccent,
       showLoader: true,
       loadingText: Text("Loading..."),
-      loaderColor: Color(0x004242),
+      loaderColor: Colors.redAccent,
     );
   }
 }
